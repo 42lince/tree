381e9de87e6533f7793f23de61ac7951439a252c
42lince <42.duxiaoxi@gmail.com>
2016-10-03 21:50:03 +0000

    basic functionality working

--- pytree.py
+++ pytree.py
@@ -1,11 +1,43 @@
 #!/usr/bin/env python3
 import subprocess
 import sys
-
+import os
 
 # YOUR CODE GOES here
 
 
-if __name__ == '__main__':
+def myTree(path):
+    result = [os.path.basename(path)]
+    if os.path.isdir(path) and os.listdir(path):
+        allContent = os.listdir(path)
+        #sort all the files in alphabetical order
+        sortedContent = sorted(allContent, key=lambda e: e.lower())
+        for i in range(len(sortedContent)):
+            if sortedContent[i][0] != '.':
+                subtree = myTree(path + "/"+ sortedContent[i])
+
+                indent = ""
+                for j in range(len(subtree)):
+                    if j == 0:
+                        if i == len(allContent) - 1:
+                            indent = "`-- "
+                        else: 
+                            indent = "|-- "
+                    elif i == len(allContent) - 1:
+                        indent = "    "
+                    else:
+                        indent = "|   "
+                    result.append(indent + subtree[j])
+    return result
+
+def prettyDisplay(tree):
+    for line in tree:
+        print(line)
+
+if len(sys.argv) > 1:
+    path = sys.argv[1]
+else:
+    path = "."
+prettyDisplay(myTree(path))
     # just for demo
-    subprocess.run(['tree'] + sys.argv[1:])
+    #subprocess.run(['tree'] + sys.argv[1:])
